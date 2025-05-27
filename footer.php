b98153fce13162a93fbe8e167dad2a846ae3edfa
Maicon Ribeiro Esteves <maicon.burn@gmail.com>
2025-05-27 08:35:25 +0100

    Remove worpress mention at the footer

--- footer.php
+++ footer.php
@@ -76,11 +76,6 @@
 
 				<div class="site-info">
 					<?php
-					/* translators: %s: CMS name, i.e. WordPress. */
-					printf( esc_html__( 'Proudly powered by %s', 'my-growth-tools' ), 'WordPress' );
-					?>
-					<span class="sep"> | </span>
-					<?php
 					/* translators: 1: Theme name, 2: Theme author. */
 					printf( esc_html__( 'Theme: %1$s by %2$s.', 'my-growth-tools' ), 'My Growth Tools', 'Maicon Ribeiro Esteves' );
 					?>
